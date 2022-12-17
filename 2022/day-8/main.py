66f668923062d665a9f57c44a9a1b19eae5addfe
Stefano Androni <65281018+stefanoandroni@users.noreply.github.com>
2022-12-18 00:32:55 +0100

    Day8 Part1 completed

--- 2022/day-8/main.py
+++ 2022/day-8/main.py
@@ -1,9 +1,80 @@
+# not optimized (bad implementation) (TODO - recursive - if vis_matrix[r][c]=True and matrix[r][c]<val: return True)
 
-INPUT_FILE_PATH = 'data/test-input.txt'
+INPUT_FILE_PATH = 'data/input.txt'
 
 def main():
     with open(INPUT_FILE_PATH, 'r') as f:
-        pass
+
+        matrix = get_matrix_from_file(f) # data_matrix
+        # print(*matrix, sep = "\n") 
+        
+        l = len(matrix)
+
+        vis_matrix = [[False for _ in range(l)] for _ in range(l)] # visibility_matrix (False by default)
+        # print(*vis_matrix, sep = "\n") 
+
+        for r in range(l):
+            for c in range(l):
+                if r in {0, l-1} or c in {0, l-1}:
+                    vis_matrix[r][c] = True 
+                else:
+                    vis_matrix[r][c] = check_visibility(r, c, matrix)
+        
+        # print(*vis_matrix, sep = "\n") 
+
+        visible_count = count_true_in_matrix(vis_matrix)
+        print(visible_count) # <Part 1>
+
+def check_visibility(r, c, matrix):
+    return (
+        check_visibility_direction(r, c, Directions.LEFT, matrix[r][c], matrix) or 
+        check_visibility_direction(r, c, Directions.RIGHT, matrix[r][c], matrix) or
+        check_visibility_direction(r, c, Directions.UP, matrix[r][c], matrix) or
+        check_visibility_direction(r, c, Directions.DOWN, matrix[r][c], matrix)
+        )
+
+def check_visibility_direction(r, c, dir, val, matrix):
+    l = len(matrix)
+
+    match dir:
+        case Directions.UP:
+            r = r-1
+        case Directions.DOWN:
+            r = r+1
+        case Directions.LEFT:
+            c = c-1
+        case Directions.RIGHT:
+            c = c+1
+
+    if 0 <= r < l and 0 <= c < l:
+        if matrix[r][c] < val:
+            return check_visibility_direction(r, c, dir, val, matrix)
+        return False
+    else:
+        return True # visibility has reached the edge
+
+def count_true_in_matrix(matrix):
+    l = len(matrix) # square matrix -> l x l
+    count = 0
+    for row in range(l):
+        for col in range(l):
+            if matrix[row][col]:
+                count += 1
+    return count
+
+def get_matrix_from_file(file):
+    f = file.read().strip()
+    matrix = []
+    lines = f.split('\n')
+    for line in lines:
+        matrix.append([eval(x) for x in list(line)])
+    return matrix
+
+class Directions:
+    UP = 'u'
+    DOWN = 'd'
+    LEFT = 'l'
+    RIGHT = 'r'
 
 if __name__ == "__main__":
     main()
