07d11a0253fe2e100d5ce05609f716387d5f135e
Stefano Androni <65281018+stefanoandroni@users.noreply.github.com>
2022-12-21 01:56:47 +0100

    Day13 Part1 completed

--- 2022/day-13/main.py
+++ 2022/day-13/main.py
@@ -1,94 +1,56 @@
-# pair of packet
-# \n
-# pair of packet
-
 import ast
 
-# You need to identify how many pairs of packets are in the right order.
-INPUT_FILE_PATH = 'data/test-input.txt'
+INPUT_FILE_PATH = 'data/input.txt'
 
 def main():
     with open(INPUT_FILE_PATH, 'r') as f:
         file = f.read().strip()
     pairs = file.split('\n\n')
 
-    right_order_pairs = []
+    S = []
+
     for index, pair in enumerate(pairs):
-        print(index + 1, "---------------------------------------------------------------------------------")
-        if is_in_right_order(pair):
-            right_order_pairs.append(index + 1)
+        p1, p2 = [ast.literal_eval(p) for p in pair.split('\n')]
+        if not compare(p1, p2) == False:
+            S.append(index + 1)
 
-    print(right_order_pairs)
-    print(sum(right_order_pairs)) # <Part 1>
+    # print(S)
+    print(sum(S)) # <Part 1>
 
-def is_in_right_order(pair):
-    pair_1, pair_2 = [ ast.literal_eval(p) for p in pair.split('\n')]
-    return is_in_right_order_recursive(pair_1, pair_2)
+def compare(left, rigth):
 
-def is_in_right_order_recursive(pair_1, pair_2):
-    left = None
-    rigth = None
-    print("*****************")
-    print("ENTER - pair1, pair2", pair_1, pair_2)
-    
-    if pair_1 is None and pair_2 is None:
-        return True
-
-    # is not None
-    if pair_1 is not None:
-        if isinstance(pair_1, list):
-            if len(pair_1) > 0:
-                left = pair_1.pop(0)
-            else:
-                pair_1 = None
-                left = None
-        else:
-            left = pair_1
-            pair_1 = None
-    else: # pair_1 is None, pair_2 is not None
-        return True
-    
-    if pair_2 is not None:
-        if isinstance(pair_2, list):
-            if len(pair_2) > 0:
-                rigth = pair_2.pop(0)
-            else:
-                pair_2 = None
-                rigth = None
-        else:
-            rigth = pair_2
-            pair_2 = None
-    else: # pair_2 is None, pair_1 is not None
-        return False
-
-    print("l", left, "r", rigth)
-
-    if isinstance(left, int) and isinstance(rigth, int): # Both values are integers
-        print("Both values are integers")
+    # both values are integers
+    if isinstance(left, int) and isinstance(rigth, int):
         if left < rigth: 
             return True
-        if left > rigth:
+        elif left > rigth:
             return False
-        return is_in_right_order_recursive(pair_1, pair_2)
+        return None
 
-    elif isinstance(left, list) and isinstance(rigth, list): # Both values are lists
-        print("Both values are lists")
-        return all(
-            [
-                is_in_right_order_recursive(
-                    left[x] if x < len(left) else None, rigth[x] if x < len(rigth) else None
-                ) for x in range(max(len(left), len(rigth)))
-            ]
-        ) and is_in_right_order_recursive(pair_1, pair_2) # TODO: keep checking even after a False return in the list (?)
-    
-    else: # exactly one value is an integer (safe condition (?))
-        print("Mixed types")
-        if isinstance(left, int):
-            pair_2 = rigth
-            return is_in_right_order_recursive([left], pair_2)
-        else:
-            pair_1 = left
-            return is_in_right_order_recursive(pair_1, [rigth])
-    
+    # both values are lists
+    if isinstance(left, list) and isinstance(rigth, list):
+        i = 0
+
+        while i < len(left) and i < len(rigth):
+            c = compare(left[i], rigth[i])
+            if c == True:
+                return True
+            if c == False:
+                return False
+            i += 1
+
+        if len(left) < len(rigth):
+            return True
+        if len(left) > len(rigth):
+            return False
+        return None
+
+    # exactly one value is an integer
+    if isinstance(left, int) and isinstance(rigth, list):
+        return compare([left], rigth)
+
+    if isinstance(left, list) and isinstance(rigth, int):
+        return compare(left, [rigth])
+
 if __name__ == "__main__":
     main()
