89e2cde5ca7f6819877ba5a128f9aff25f539d0c
Stefano Androni <65281018+stefanoandroni@users.noreply.github.com>
2022-12-21 02:24:55 +0100

    Day13 Part2 completed

--- 2022/day-13/main.py
+++ 2022/day-13/main.py
@@ -1,31 +1,48 @@
 import ast
+from functools import cmp_to_key
 
 INPUT_FILE_PATH = 'data/input.txt'
 
 def main():
     with open(INPUT_FILE_PATH, 'r') as f:
         file = f.read().strip()
+    
+    # Part 1 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     pairs = file.split('\n\n')
 
     S = []
 
     for index, pair in enumerate(pairs):
         p1, p2 = [ast.literal_eval(p) for p in pair.split('\n')]
-        if not compare(p1, p2) == False:
+        if compare(p1, p2) in {-1, 0}:
             S.append(index + 1)
 
     # print(S)
     print(sum(S)) # <Part 1>
+    
+    
+    # Part 2 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    pairs = [ast.literal_eval(p) for p in file.replace('\n\n', '\n').split('\n')]
+    pairs.append([[2]])
+    pairs.append([[6]])
+
+    pairs = sorted(pairs, key = cmp_to_key(lambda p1, p2: compare(p1, p2)))
+    # print(*pairs, sep='\n')
+   
+    d_p1 = pairs.index([[2]]) + 1
+    d_p2 = pairs.index([[6]]) + 1
+    d_key = d_p1 * d_p2
+    print(d_key) # <Part 2>
 
 def compare(left, rigth):
 
     # both values are integers
     if isinstance(left, int) and isinstance(rigth, int):
         if left < rigth: 
-            return True
+            return -1
         elif left > rigth:
-            return False
-        return None
+            return 1
+        return 0
 
     # both values are lists
     if isinstance(left, list) and isinstance(rigth, list):
@@ -33,17 +50,17 @@ def compare(left, rigth):
 
         while i < len(left) and i < len(rigth):
             c = compare(left[i], rigth[i])
-            if c == True:
-                return True
-            if c == False:
-                return False
+            if c == -1:
+                return -1
+            if c == 1:
+                return 1
             i += 1
 
         if len(left) < len(rigth):
-            return True
+            return -1
         if len(left) > len(rigth):
-            return False
-        return None
+            return 1
+        return 0
 
     # exactly one value is an integer
     if isinstance(left, int) and isinstance(rigth, list):
