e737bc9eca958c06719e461e60201c25acb5f32b
Stefano Androni <65281018+stefanoandroni@users.noreply.github.com>
2023-02-12 16:57:38 +0100

    Code refactoring

--- 2022/day-1/main.py
+++ 2022/day-1/main.py
@@ -1,7 +1,7 @@
-INPUT_FILE_PATH = '../data/test-input.txt'
+INPUT_FILE_PATH = 'data/test-input.txt'
 
 def main():
-    with open('data/test-input.txt', 'r') as f:
+    with open(INPUT_FILE_PATH, 'r') as f:
         list = [[int(y) for y in x.split("\n") if y] for x in f.read().split("\n\n")]
     
     sum_list = [sum(el) for el in list]
