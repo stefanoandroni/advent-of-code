878797cbb92a0a2e6ee898ced52ebe2022257ebf
Stefano Androni <65281018+stefanoandroni@users.noreply.github.com>
2023-12-09 15:35:31 +0100

    Day9 Part2 completed

--- 2023/day-9/main.py
+++ 2023/day-9/main.py
@@ -1,26 +1,33 @@
 
 import re
 
-INPUT_FILE_PATH = '../data/input.txt'
+INPUT_FILE_PATH = 'data/test-input.txt'
 
 
 def main():
     H = parse_input_file() # H: H[i] = history list for i-value
 
+    # Part 1
     sum = 0
     for h in H:
         sum += predict_value(h)
     
-    # Part 1
+    print(sum)
+
+    # Part 2
+    sum = 0
+    for h in H:
+        h.reverse()
+        sum += predict_value(h)
+    
     print(sum)
 
 def predict_value(h):
     # Base Case
     if all(x == 0 for x in h):
         return 0
-    
+    # Recursive Case    
     h_diff = [h[i + 1] - h[i] for i in range(len(h) - 1)]
-    
     return h[-1] + predict_value(h_diff) 
     
 
