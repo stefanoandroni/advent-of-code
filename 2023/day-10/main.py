22a39aa1e63953b453421791e83b1edf670dbe43
Stefano Androni <65281018+stefanoandroni@users.noreply.github.com>
2023-12-10 21:34:06 +0100

    Code refactoring

--- 2023/day-10/main.py
+++ 2023/day-10/main.py
@@ -1,5 +1,8 @@
 
-INPUT_FILE_PATH = 'data/input.txt'
+import math
+
+
+INPUT_FILE_PATH = 'data/test-input.txt'
 
 DIR_TO_COORDS = {
     'N': (0, -1),
@@ -31,31 +34,28 @@ def main():
     xs, ys = S
     cell1, cell2 = get_starting_cells(xs, ys)
 
+    current_cell = cell1 # or cell2
+
     visited_cells.append(S)
     visited_cells.append(cell1)
-    visited_cells.append(cell2)
 
-    path_length = 1
-    while cell1 != cell2:
-        cell1 = get_next_cell(cell1)
-        visited_cells.append(cell1)
-
-        cell2 = get_next_cell(cell2)
-        visited_cells.append(cell2)
-
+    path_length = 1 # S -> cell1
+    
+    while current_cell != None:
+        current_cell = get_next_cell(current_cell)
+        visited_cells.append(current_cell)
         path_length += 1
 
     # Part 1
-    print(path_length)
+    print(math.ceil(path_length / 2))
     
 
 def get_next_cell(current_cell):
     x, y = current_cell
     coords = get_coords_from_dirs(M[y][x])
-    candidate_cells = [(x + xc, y + yc) for xc, yc in coords if (x + xc, y + yc) not in visited_cells]
+    next_cell = [(x + xc, y + yc) for xc, yc in coords if (x + xc, y + yc) not in visited_cells]
     # assert: 0 <= len(candidate_cells) <= 1 
-    return candidate_cells[0] if len(candidate_cells) > 0 else visited_cells[-1] # else: path2 ends up in cell1 (cell1 = visited_cells[-1])
-
+    return next_cell[0] if len(next_cell) > 0 else None # else: next cell is S
 
 def get_coords_from_dirs(dirs):
     return [DIR_TO_COORDS[dir] for dir in dirs]
@@ -65,9 +65,10 @@ def get_starting_cells(xs, ys):
     starting_cells = set()
     for xd, yd in DIR_TO_COORDS.values():
         xds, yds = xs + xd, ys + yd
-        for x, y  in get_coords_from_dirs(M[yds][xds]):
-            if (xds+x, yds+y) == (xs, ys):
-                starting_cells.add((xds, yds))
+        if M[yds][xds] in SYMBOL_TO_DIRS.values():
+            for x, y  in get_coords_from_dirs(M[yds][xds]):
+                if (xds+x, yds+y) == (xs, ys):
+                    starting_cells.add((xds, yds))
     # assert: len(starting_cells) == 2 
     return starting_cells
 
